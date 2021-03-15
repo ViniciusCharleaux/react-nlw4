012ab5c262e7dd57a07f4548bc36b91f6c29191b
ViniciusCharleaux <vinicius.charleaux@gmail.com>
2021-03-15 20:07:36 -0300

    Estilos e componente CompletedChallenges

---
+++ moveit-next/src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallenges.module.css';
+
+export function CompletedeChallenges(){
+    return (
+        <div className={styles.completedChallengesConteiner}>
+            <span> Desafios completos </span>
+            <span> 5 </span>
+        </div>
+    )
+}
--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,11 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+import { FaArrowCircleUp } from 'react-icons/fa';
+
 export function Profile(){
     return (
-        <div>
-            <img src="https://github.com/ViniciusCharleaux" alt="Vinicius Charleaux"></img>
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/ViniciusCharleaux.png" alt="Vinicius Charleaux"></img>
             <div>
                 <strong> Vinicius Charleaux </strong>
-                <p>Level 1</p>
+                <p>
+                    <span> <FaArrowCircleUp /> </span>
+                    Level 1
+                </p>
             </div>
         </div>
-    )
+    ) 
 }
