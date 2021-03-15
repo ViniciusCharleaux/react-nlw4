38d55ff18a28d9de1fc8e3db7b7575791acae9fc
ViniciusCharleaux <vinicius.charleaux@gmail.com>
2021-03-15 19:15:41 -0300

    modulos css e componente perfil

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/ViniciusCharleaux" alt="Vinicius Charleaux"></img>
+            <div>
+                <strong> Vinicius Charleaux </strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import { ExperienceBar } from "../components/ExperienceBar";
-import Head from 'next/head';
+import { Profile } from "../components/Profile";
 
 export default function Home() {
   return (
     <div className="container">
       <ExperienceBar/>
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
