513663f60d7fd18c6e7993da12336875d646001c
ViniciusCharleaux <vinicius.charleaux@gmail.com>
2021-03-15 23:49:24 -0300

    Componente e estililos do Countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,18 +1,24 @@
 import { CompletedeChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import Head from 'next/head';
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <ExperienceBar/>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
 
+      <ExperienceBar/>
       <section>
         <div>
           <Profile />
           <CompletedeChallenges />
+          <Countdown />
         </div>
 
         <div>
